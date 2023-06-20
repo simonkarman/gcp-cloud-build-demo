1ac529af3723273308841d1f8e61d7688504df0c
Simon Karman <simon.karman@xebia.com>
2023-06-20 23:10:27 +0200

    Revert "Added hello to response"
    This reverts commit 3118142b91e686a5792a7a23d067f5bd6494171a.

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,5 @@ import express, { Request, Response } from 'express';
 export const app = express();
 app.use(express.json());
 app.get('/', (req: Request, res: Response) => {
-  res.send('Server running NodeJS for the demo of Google Cloud Build! Hello!');
+  res.send('Server running NodeJS for the demo of Google Cloud Build');
 });
